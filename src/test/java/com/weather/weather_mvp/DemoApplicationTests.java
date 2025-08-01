11da15c8227c2b13df7b9fa97c33a1306ef80ada
andreilopatkin <andrew4635@yandex.ru>
2025-08-01 14:24:57 +0300

    /

---
+++ src/test/java/com/weather/weather_mvp/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.weather_mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
