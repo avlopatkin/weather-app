aad4f0e58a77283cbc5f548221a2f9cadb978ebd
andreilopatkin <andrew4635@yandex.ru>
2025-08-11 22:13:14 +0300

    Создал репозитории

---
+++ src/main/java/com/weather/weather_mvp/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.weather.weather_mvp.repository;
+
+import com.weather.weather_mvp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByLogin(String login);
+}
